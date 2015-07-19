12a0b0299a464cdd3ba4c06bd3c17beb16c2dddc
Alston <and_croning@hotmail.com>
2015-07-19 00:00:05 -0400

    Using Strings in Java

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Dishan Croning on 7/18/2015.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        //System.out.println("Hello World");
+        String firstname = "John";
+        String fullname = firstname + " Jimmy";
+        System.out.println(fullname.indexOf('y'));
+    }
+}
