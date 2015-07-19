a1b838b03f309f082664f638af2fb548a2199091
Alston <and_croning@hotmail.com>
2015-07-19 01:16:58 -0400

    Preparing Files for Classes with Java

---
+++ Classes_in_java/src/Classes.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Dishan Croning on 7/20/2015.
+ */
+public class Classes {
+
+}
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,28 @@
 /**
  * Created by Dishan Croning on 7/18/2015.
  */
+
+//Arrays in Java
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {//String[] args is also an array
+
+        int[] numbers = new int[10];
+
+        numbers[0] = 5;
+        numbers[1] = 8;
+
+        System.out.println(numbers[0]);//answer is 5
+        System.out.println(numbers[1]);//answer is 8
+        System.out.println(numbers[2]);//answer is 0
+        //answer is 0 because it initializes the default value is 0
+
+    }
+}
+
+/*
+//Strings Example
 public class HelloWorld
 {
     public static void main(String[] args)
@@ -11,3 +33,4 @@ public class HelloWorld
         System.out.println(fullname.indexOf('y'));
     }
 }
+*/
