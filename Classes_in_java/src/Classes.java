5ca652f8524cad5984d024a33d8c52bfaafd8d0c
Alston <and_croning@hotmail.com>
2015-07-21 00:38:07 -0400

    Adding the Correct file to the project

--- Classes_in_java/src/Classes.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Dishan Croning on 7/20/2015.
- */
-public class Classes {
-
-}
---
+++ Classes_in_java/src/printing/printer.java
@@ -0,0 +1,7 @@
+package printing;
+
+/**
+ * Created by Dishan Croning on 7/21/2015.
+ */
+public class printer {
+}
